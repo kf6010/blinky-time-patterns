0215e3c6ebe6b2e4b348e7eb89f65fddf721d506
Dr Alun Moon <alun.moon@northumbria.ac.uk>
2019-10-17 14:29:46 +0100

    Tidy up environment

--- src/main.cpp
+++ src/main.cpp
@@ -1,26 +1,26 @@
 #include "mbed.h"
- 
+
 /* for pinnames see https://os.mbed.com/teams/Freescale/wiki/frdm-k64f-pinnames
  */
 
 /* Create a Digital-output for the K64F Red led
  * The initial value defaults to 0 -- led is on
  */
-DigitalOut red(LED_RED); 
- 
+DigitalOut red(LED_RED);
 
-void toggle(void) 
+
+void toggle(void)
 {
-	red = !red;
+    red = !red;
 }
 
 /* Create a periodic interrupt */
 Ticker tmr;
 
 int main() {
-	tmr.attach( toggle , 0.5);
+    tmr.attach( toggle, 0.5);
 
     while (true) {  /* while _super-loop_ */
     }
 }
- 
+
